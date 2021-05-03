bbeef78430e259c2867b933d41f8805a84a7b440
Jaclyn French <jaclynmariefrench@gmail.com>
2021-05-03 13:43:08 -0500

    up to chapter 8

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -1,11 +1,15 @@
-import { getStyles } from "./database.js";
+import { getStyles, setStyle } from "./database.js";
 
 const styles = getStyles();
 
-document.addEventListener("change", (event) => {});
+document.addEventListener("change", (event) => {
+  if (event.target.name === "style") {
+    setStyle(parseInt(event.target.value));
+  }
+});
 
 export const JewelryStyles = () => {
-let html = ""
+  let html = "";
   // Use .map() for converting objects to <li> elements
   const listItemsArray = styles.map((style) => {
     return `<div>
@@ -14,6 +18,6 @@ let html = ""
   });
   // Join all of the strings in the array into a single string
 
-  html += listItemsArray.join(" ") ;
+  html += listItemsArray.join(" ");
   return html;
 };
--- scripts/Orders.js
+++ scripts/Orders.js
@@ -2,24 +2,18 @@ import { getOrders } from "./database.js"
 
 const buildOrderListItem = (order) => {
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} costs ${order.totalValue}
     </li>`
 }
-
 export const Orders = () => {
     /*
         Can you explain why the state variable has to be inside
         the component function for Orders, but not the others?
     */
     const orders = getOrders()
-
     let html = "<ul>"
-
     const listItems = orders.map(buildOrderListItem)
-
     html += listItems.join("")
     html += "</ul>"
-
     return html
 }
-
